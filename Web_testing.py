e0d80afe522352540945668f3e3c5e5ca29c01c0
NattheCh <nataliachamerska@gmail.com>
2020-06-23 22:52:58 +0200

    Web testing

--- Web_testing.py
+++ Web_testing.py
@@ -1,18 +1,8 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get("http://automationpractice.com")
 driver.maximize_window()
-driver.find_element_by_id("search_query_top")
-driver.find_element(By.ID, "search_query_top")
-driver.find_element_by_css_selector("a")
-driver.find_element_by_css_selector("img.logo")
-driver.get("http://automationpractice.com/index.php?id_product=6&controller=product")
-print(driver.find_element_by_css_selector("td:nth-child(2)").text)
-print(driver.find_element_by_xpath("//tbody//tr[2]//td[2]").text)
-lista = len(driver.find_elements_by_xpath("//tbody//tr"))
-print(lista)
-
-driver.quit()
+driver.find_element_by_id("search_query_top").send_keys("skirt", Keys.ENTER)
